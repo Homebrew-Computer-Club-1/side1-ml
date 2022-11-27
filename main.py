d146b92b696c9584361c707d3ea1bdacddde4a7f
LDW <leehan02@hanyang.ac.kr>
2022-11-27 18:57:30 +0900

    model

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.cluster import KMeans
 from numpy import dot
 from numpy.linalg import norm
+from sklearn.metrics.pairwise import cosine_similarity
 import uvicorn
 
 app = FastAPI()
@@ -58,51 +59,41 @@ class DataPreprocessing(object):
         
         return df
 
+
+# sklearn의 cosine_similarity 사용
+# 코사인 유사도 계산 시 시간복잡도 문제 해결
+
 class Classification(object):
     
     def __init__(self, df_data):
         self.df = df_data
+        self.n_samples = self.df.shape[0]
     
-    def Extraction(self):
-        # 가장 선호하는 feature 추출
-        self.ls = []
-        for i in range(0, self.df.shape[0]):
-            self.ls.append(self.df.sort_values(by=[i], axis=1).iloc[i, -2:].index.tolist())
-            
-        # 각 이용자의 구독 채널 수 추출
-        arr = np.array(self.df)
-        self.num_ch = np.sum(arr, axis=1).reshape(-1, 1)
-        
-    def Ohe(self): # 각 샘플에 대해 추출한 가장 선호하는 2개의 특성에 대해 원핫인코딩
+    def Ohe(self, df): # 각 샘플에 대해 추출한 가장 선호하는 2개의 특성에 대해 원핫인코딩
         ohe = OneHotEncoder(sparse=False)
-        return ohe.fit_transform(self.ls)
+        self.train_feature = ohe.fit_transform(np.array(df[['first_category', 'second_category']]).reshape(-1,1))
     
-    def makeData(self, train_feature):
-        self.X_train = np.hstack((self.num_ch, train_feature))
+    def makeData(self, channels):
+        channels = np.array(channels).reshape(self.n_samples, -1)
+        self.X_train = np.hstack((channels, np.array(self.train_feature).reshape(self.n_samples, -1)))
         
     def Kmeans(self): # k_means 클러스터링
-        model = KMeans(n_clusters=16)
+        model = KMeans(n_clusters=2)
         model.fit(self.X_train)
         return np.array(model.predict(self.X_train))
-
-    def cosine_similarity(self, pt1, pt2):
-      return dot(pt1, pt2) / (norm(pt1) * norm(pt1))
-      
-    def ReturnResult_cosine_similarity(self, k=5): 
+    
+    def ReturnResult_cosine_similarity(self, k=1):
         
         k_users = np.empty((0,k))
         for i in range(0, self.X_train.shape[0]):
-            distance = []
-            for j in range(0, self.X_train.shape[0]):
-                if i != j:
-                    distance.append(self.cosine_similarity(self.X_train[i, :], self.X_train[j, :]))
-                if j == (self.X_train.shape[0] - 1):
-                    # i 번째 데이터 포인터와 다른 데이터들 사이의 거리를 작은순으로 정렬한 후 해당 인덱스를 이용해서 가장 가까운 k명의 이용자 선별
-                    distance_idx = np.array(distance).argsort()
-                    k_users = np.append(k_users, [distance_idx[:k]], axis=0)
+            distance = [cosine_similarity(self.X_train[i].reshape(1,-1), self.X_train)]
+            
+            # i 번째 데이터 포인터와 다른 데이터들 사이의 거리를 작은순으로 정렬한 후 해당 인덱스를 이용해서 가장 가까운 k명의 이용자 선별
+            distance_idx = np.array(distance).ravel().argsort()
+            k_users = np.append(k_users, [distance_idx[:k]], axis=0)
                     
         return k_users
-
+        
 
 class reqBody(BaseModel):
     youtubeSubscriptionData : List[List[str]]
